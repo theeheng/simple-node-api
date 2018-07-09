b981e752f7f4cc59a35e485ff74d0e8f0a14f093
HengTan <heng.tan@fourth.com>
2018-07-09 11:52:50 +0100

    update api to add search functionality
    update api to add search functionality

--- app/routes/album_routes.js
+++ app/routes/album_routes.js
@@ -1,9 +1,26 @@
 // note_routes.js
 const data =  require('./albums.json');
+const _ = require('lodash');
 
 module.exports = function(app, db) {
   app.get('/albums', (req, res) => {
     // You'll create your note here.
     res.send(data)
+  });
+
+  app.get('/search', (req, res) => {
+    // You'll create your note here.
+
+    if(req.query['name'])
+    {
+		var filtered = _.filter(data, function(album){
+    return album.title.indexOf(req.query['name'])>-1;
+    });
+		res.send(filtered)
+	}
+    else
+    {
+    	res.send(data)
+	}
   });
 };
